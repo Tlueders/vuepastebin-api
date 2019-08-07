5263b34efebd3e5afc5ffea5272e63e774895f2f
tannerlueders <tlueders@allbound.com>
2019-08-07 16:19:57 -0700

    inital commit

---
+++ models/pastes.js
@@ -0,0 +1,31 @@
+var Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+  }
+);
+
+var Paste = sequelize.define('paste', {
+    page_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    paste: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    language: {
+        type: Sequelize.STRING
+    }
+});
+
+sequelize.sync()
+    .then(() => console.log('pastes table has been successfully created, if one doesn\'t exist'))
+    .catch(error => console.log('This error occured', error));
+
+module.exports = Paste;
